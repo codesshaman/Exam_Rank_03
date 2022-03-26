46cc83bf1e50fdcc3ab4b21cef26e30ff965fe31
Jimmie Leslee <jleslee@et-h4.kzn.21-school.ru>
2022-03-26 12:54:35 +0300

    modifed micro

--- my_micro_paint.c
+++ my_micro_paint.c
@@ -21,7 +21,7 @@ int draw(void) {
 		return -1;
 	for (int x = 0; x < h; x++) {
 		for (int y = 0; y < w; y++) {
-			bool cond1 = !(x < xs || (xs + xw) < x || y < ys  || (ys + yh) < y);
+			bool cond1 = !(x < xs || (xs + xw) < x || y < ys || (ys + yh) < y);
 			bool cond2 = x - xs < 1 || (xs + xw) - x < 1 || y - ys < 1 || (ys + yh) - y < 1;
 			if ((cond1) && ((cond2 && type == 'r') || type == 'R'))
 				tab[x][y] = c;
