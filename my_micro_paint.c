adbf7fa01d110a763fb199025de1280bf54d12f8
Bick <90702473+codesshaman@users.noreply.github.com>
2022-03-26 18:06:03 +0300

    work code

--- my_micro_paint.c
+++ my_micro_paint.c
@@ -1,53 +1,58 @@
-#include <string.h>
 #include <unistd.h>
-#include <stdlib.h>
 #include <stdio.h>
-#include <stdbool.h>
-#define ARGS_ERR "Error: argument"
-#define FILE_ERR "Error: Operation file corrupted"
-char **tab, c, type;
-int w, h, res;
-float xs, ys, xw, yh;
-FILE *fd;
-
-void putstr(char *s) {
-	while (*s)
-		write(1, s++, 1);
-	write(1, "\n", 1);
+int ft_error(FILE *file){
+	write(1, "Error: Operation file corrupted\n", 32);
+	fclose(file);
+	return 1;
 }
-
-int draw(void) {
-	if ((type != 'r' && type != 'R') || xw <= 0 || yh <= 0)
-		return -1;
-	for (int x = 0; x < h; x++) {
-		for (int y = 0; y < w; y++) {
-			bool cond1 = !(x < xs || (xs + xw) < x || y < ys || (ys + yh) < y);
-			bool cond2 = x - xs < 1 || (xs + xw) - x < 1 || y - ys < 1 || (ys + yh) - y < 1;
-			if ((cond1) && ((cond2 && type == 'r') || type == 'R'))
-				tab[x][y] = c;
+int ft_check(int i, int j, float x, float y, float w, float h){
+	if(i < y || i > y +h || j < x || j > x + w)
+		return 0;
+	if(i - y < 1 || y + h - i < 1 || j - x < 1 || x + w - j < 1)
+		return 2;
+	return 1;
+}
+int main(int argc, char **argv){
+	FILE *file;
+	int i = -1, j, height, width, arg;
+	float x, y, w, h;
+	char back, c, symb;
+	if(argc != 2)
+		return(write(1, "Error: argument\n", 16) && 1);
+	if(!(file = fopen(argv[1], "r")))
+		return(ft_error(file));
+	if((arg = fscanf(file, "%d %d %c\n", &width, &height, &back)) != 3)
+		return(ft_error(file));
+	if(height <= 0 || height > 300 || width <= 0 || width > 300)
+		return(ft_error(file));
+	char arr[height][width];
+	while (++i < height){
+		j = -1;
+		while (++j < width)
+			arr[i][j] = back;
+	}
+	while((arg = fscanf(file, "%c %f %f %f %f %c\n", &c, &x, &y, &w, &h, &symb)) == 6){
+		if((c != 'r' && c != 'R') || w <= 0 || h <= 0)
+			return(ft_error(file));
+		i = -1;
+		while (++i < height)
+		{
+			j = -1;
+			while (++j < width)
+			{
+				if((ft_check(i, j, x, y, w, h) == 2 && c == 'r') || (ft_check(i, j, x, y, w, h) && c  == 'R'))
+					arr[i][j] = symb;
+			}
 		}
 	}
-	return 0;
-}
-
-int main(int ac, char **av) {
-	if (ac != 2)
-		return (putstr(ARGS_ERR), 1);
-	fd = fopen(av[1], "r");
-	if (!fd)
-		return (putstr(FILE_ERR), 1);
-	res = fscanf(fd, "%d %d %c\n", &w, &h, &c);
-	if (res != 3 || w <= 0 || h <= 0 || w > 300 || h > 300)
-		return (putstr(FILE_ERR), 1);
-	tab = calloc(h, sizeof(char *));
-	for (int i = 0; i < h; i++) {
-		tab[i] = calloc(w + 1, 1);
-		memset(tab[i], c, w);
+	if(arg > 0 && arg !=6)
+		return(ft_error(file));
+		i = -1;
+	while(++i < height){
+		j = -1;
+		while(++j < width)
+			write(1, &arr[i][j], 1);
+		write(1, "\n", 1);
 	}
-	while ((res = fscanf(fd, "%c %f %f %f %f %c\n", &type, &xs, &ys, &xw, &yh, &c)) != -1)
-		if (res != 6 || draw() == -1)
-			return (putstr(FILE_ERR), 1);
-	for (int i = 0; i < h; i++)
-		putstr(tab[i]);
-	fclose(fd);
+	return(fclose(file) && 0);
 }
